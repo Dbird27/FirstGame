966e7d33505f1e5abfd0738c283f1e2d7665d3b2
Dylan <dylangotrent@gmail.com>
2024-12-27 21:19:50 -0500

    Updated code in a few spots, namely tried to implement auto move(not completeted yet) and implemented a snake body.

--- base_code.py
+++ base_code.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import copy
 import time
 
 #Setup Pygame
@@ -12,7 +13,8 @@ dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2 , screen.get_height() / 2)
 player_size = 20
-player_speed = 60
+player_character = [player_pos]
+snake_head = player_pos
 
 grid_size = player_size*2
 grid_color = "dark green"
@@ -21,15 +23,16 @@ apple_exists = False
 apple_pos = pygame.Vector2(0,0)
 apple_color = "red"
 
-#For some logic later done, positing for up/right, negative for left/down
-direction = 1
+#For some logic later down, positing for up/right, negative for left/down
+direction =""
 score = 0
+wait_count = 0
 
 #create a method to spawn apples
 def spawnApple():
     global apple_pos, apple_exists
-    x_pos = random.randint(0, width // 40)
-    y_pos = random.randint(0, height // 40)
+    x_pos = random.randint(0, (width-40) // 40)
+    y_pos = random.randint(0, (height-40) // 40)
     apple_pos = pygame.Vector2(x_pos * 40 + 20, y_pos * 40 + 20)
     apple_exists = True
 
@@ -41,63 +44,76 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    if player_pos == apple_pos:
+    if snake_head == apple_pos:
         apple_exists = False
         score +=1
         print(score)
+    else:
+        player_character.pop()
 
     if not apple_exists:
         spawnApple()
 
+    #Handle player pressing key
+    keys = pygame.key.get_pressed()
+    new_head = copy.copy(snake_head)
+    if keys[pygame.K_w]:
+        new_head.y -= grid_size
+        direction = "up"
+    elif keys[pygame.K_s]:
+        new_head.y += grid_size
+        direction = "down"
+    elif keys[pygame.K_a]:
+        new_head.x -= grid_size
+        direction = "left"
+    elif keys[pygame.K_d]:
+        new_head.x += grid_size
+        direction = "right"
+    else:
+        wait_count+=1
+
+    #Handle snake moving on it's own
+    if wait_count == 2:
+        if direction == "up":
+            new_head.y -= grid_size
+        elif direction == "down":
+            new_head.y += grid_size
+        elif direction == "left":
+            new_head.x -= grid_size
+        else:
+            new_head.x += grid_size
+    
+
+    #Update player character
+    player_character.insert(0,new_head)
+    snake_head = player_character[0]
+
+    #Force player to stay on screen
+    if snake_head.x > width - player_size:
+        snake_head.x = width - player_size
+    if snake_head.x < player_size:
+        snake_head.x = player_size
+    if snake_head.y > height - player_size:
+        snake_head.y = height - player_size
+    if snake_head.y < player_size:
+        snake_head.y = player_size
+
     #Fill screen with color to wipe away things from last frame
     screen.fill("forest green")
-
+    #Draw the grid
     for x in range(0, width, grid_size):
         pygame.draw.line(screen, grid_color, (x,0), (x,height))
     for y in range(0, height, grid_size):
         pygame.draw.line(screen, grid_color, (0,y), (width, y))
 
     #Render game here
-    pygame.draw.circle(screen, "salmon", player_pos, player_size)
+    for part in player_character:
+        pygame.draw.circle(screen, "salmon", part, player_size)
     pygame.draw.circle(screen, apple_color, apple_pos, player_size)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= player_speed * dt
-        direction = -1
-    if keys[pygame.K_s]:
-        player_pos.y += player_speed * dt
-        direction = 1
-    if keys[pygame.K_a]:
-        player_pos.x -= player_speed * dt
-        direction = -1
-    if keys[pygame.K_d]:
-        player_pos.x += player_speed * dt
-        direction = 1
-
-    #Force player to stay on screen
-    if player_pos.x > width - player_size:
-        player_pos.x = width - player_size
-    if player_pos.x < player_size:
-        player_pos.x = player_size
-    if player_pos.y > height - player_size:
-        player_pos.y = height - player_size
-    if player_pos.y < player_size:
-        player_pos.y = player_size
-    
-    #Force player to remain in center of square
-    if player_pos.x % player_size != 0:
-        player_pos.x = round(player_pos.x/player_size)*player_size
-        if player_pos.x % grid_size == 0:
-            player_pos.x += player_size * direction
-    if player_pos.y % player_size != 0:
-        player_pos.y = round(player_pos.y/player_size)*player_size
-        if player_pos.y % grid_size == 0:
-            player_pos.y += player_size * direction
 
     #flip() display to put work on screen
     pygame.display.flip()
 
-    dt = clock.tick(60)/100 #Limits FPS to 60.
+    clock.tick(10) #Limits FPS to 60.
 
 pygame.quit()
